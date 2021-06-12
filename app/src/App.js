64127ea4eaf0e62fa39f7a8c36f209d763c3ddc6
KShivendu <shivendu@iitbhilai.ac.in>
2021-06-12 09:25:51 +0000

    Setup Material UI

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,46 @@
+import React, { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
+import { Checkbox } from '@material-ui/core'
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { darkTheme, lightTheme } from './theme.js';
+
 
 function App() {
+  const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const isDark = localStorage.getItem("darkMode")
+    ? localStorage.getItem("darkMode") === "true"
+    : prefersDark;
+  const [darkMode, setDarkMode] = useState(isDark);
+
+  function toggleDarkMode() {
+    setDarkMode(!darkMode);
+    localStorage.setItem("darkMode", !darkMode);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <CssBaseline />
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {darkMode ? "Dark" : "Light"}
+          <Checkbox checked={darkMode} onClick={() => toggleDarkMode()} />
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ app/src/theme.js
@@ -0,0 +1,120 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+import { blue } from "@material-ui/core/colors";
+
+
+const isDarkTheme = true;
+
+// COMMON 
+const overrides = {
+    MuiTypography: {
+        body2: {
+            fontFamily: "Comfortaa",
+        },
+    },
+    MuiPaper: {
+        root: {
+            boxShadow: "none",
+        },
+    },
+};
+
+const themeOptions = {
+    typography: {
+        fontFamily: [
+            "-apple-system",
+            "BlinkMacSystemFont",
+            "Roboto",
+            '"Helvetica Neue"',
+            "Arial",
+            "sans-serif",
+            '"Apple Color Emoji"',
+            '"Segoe UI Emoji"',
+            '"Segoe UI Symbol"',
+        ].join(","),
+        subtitle1: {
+            fontWeight: 'bold'
+        }
+    },
+    props: {
+        MuiLink: {
+            color: "secondary",
+        },
+        MuiTextField: {
+            variant: "outlined",
+        },
+    },
+}
+
+// DARK MODE 
+const darkPalette = {
+    type: "dark",
+    background: {
+        default: '#282c34',
+        paper: "#181b20",
+    },
+    primary: {
+        main: "rgb(26, 28, 34)",
+    },
+    secondary: {
+        main: blue["A100"],
+    },
+};
+
+const darkOverrides = {
+    ...overrides,
+    MuiChip: {
+        outlined: {
+            color: "#c8def1",
+        },
+    },
+    MuiTypography: {
+        subtitle1: {
+            color: '#769ee7'
+        }
+    }
+};
+
+// LIGHT MODE 
+
+const lightPalette = {
+    type: "light",
+    primary: { main: blue["700"] },
+    secondary: { main: blue["600"] },
+    background: {
+        default: '#f9fafb',
+        paper: "#ffffff",
+    },
+
+};
+const lightOverrides = {
+    ...overrides,
+    MuiChip: {
+        outlined: {
+            color: "#143d66",
+        },
+    },
+    MuiPaper: {
+        elevation3: {
+            boxShadow: `0 4px 6px -1px rgba(0,0,0,.1),
+                  0 2px 4px -1px rgba(0,0,0,.06)`
+        }
+    },
+    MuiTypography: {
+        subtitle1: {
+            color: '#2674c2'
+        }
+    }
+};
+
+export const darkTheme = createMuiTheme({
+    ...themeOptions,
+    palette: darkPalette,
+    overrides: darkOverrides,
+});
+
+export const lightTheme = createMuiTheme({
+    ...themeOptions,
+    palette: lightPalette,
+    overrides: lightOverrides,
+});
