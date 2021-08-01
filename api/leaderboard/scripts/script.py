e6ebda1da2a04ada2a79e0f65e3a88db3c8c646e
Arshpreet Singh <arshpreets2807@gmail.com>
2021-08-01 14:26:58 +0530

    Converted script into function

--- api/leaderboard/scripts/script.py
+++ api/leaderboard/scripts/script.py
@@ -3,49 +3,45 @@ from bs4 import BeautifulSoup
 from datetime import date
 from datetime import timedelta
 from requests.auth import HTTPBasicAuth
-repo_names = requests.get('https://api.github.com/users/OpenLake/repos',auth = HTTPBasicAuth('Username',"Password"))
 
 
-repo_list = []
-# # These are the names of the repo
-# for rep in repo_names.json():
-# 	print(f'The repo {rep["name"]} is added to the list')
-# 	print()
-# 	# Save these names in the list
-# 	repo_list.append(rep["name"])
+def Open_lake_commits():
+	repo_names = requests.get('https://api.github.com/users/OpenLake/repos',auth = HTTPBasicAuth('Username',"Password"))
 
-today = date.today()
-days = []
-# To make the range of dates
-print("You want to see the commits for the last", end = " ")
-print("days")
-dates = int(input())
 
-print()
+	repo_list = []
+	# # These are the names of the repo
 
-for z in range(dates):
-	diff = str(today - timedelta(days = z))
-	print(f'ON {diff}')
-	print()
-	#To get the commit list FOR EACH REPOSITORY
-	for reponame in repo_list:
-		gh_user = {}
-		commits_that_day = 0
-		commit = requests.get(f"https://api.github.com/repos/OpenLake/{reponame}/commits", auth = HTTPBasicAuth('Username',"Password"))
-		for x in commit.json():
-			if x["commit"]["author"]["date"][0:10] == str(diff):
-				commits_that_day = commits_that_day + 1
-				if x['commit']['author']['name'] not in gh_user:
-					gh_user[x['commit']['author']['name']] = 1
-				else:
-					gh_user[x['commit']['author']['name']] += 1 
+	today = date.today()
+	days = []
+	# To make the range of dates
+	print("You want to see the commits for the last", end = " ")
+	print("days")
+	dates = int(input("\n))
 
-		if commits_that_day:
-			print(f'There were a total of {commits_that_day} commits on {reponame}!')
+	for z in range(dates):
+		diff = str(today - timedelta(days = z))
+		print(f'ON {diff}')
+		print()
+		#To get the commit list FOR EACH REPOSITORY
+		for reponame in repo_list:
+			gh_user = {}
+			commits_that_day = 0
+			commit = requests.get(f"https://api.github.com/repos/OpenLake/{reponame}/commits", auth = HTTPBasicAuth('Username',"Password"))
+			for x in commit.json():
+				if x["commit"]["author"]["date"][0:10] == str(diff):
+					commits_that_day = commits_that_day + 1
+					if x['commit']['author']['name'] not in gh_user:
+						gh_user[x['commit']['author']['name']] = 1
+					else:
+						gh_user[x['commit']['author']['name']] += 1 
+
+			if commits_that_day:
+				print(f'There were a total of {commits_that_day} commits on {reponame}!')
+				print()
+			commits_that_day = 0
+			for m in gh_user:
+				print(f'{m} made {gh_user[m]} commits to {reponame}')
 			print()
-		commits_that_day = 0
-		for m in gh_user:
-			print(f'{m} made {gh_user[m]} commits to {reponame}')
-		print()
-		print()
-	
+			print()
+
