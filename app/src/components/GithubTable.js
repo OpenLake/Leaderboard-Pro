20eb4b3e4ac1f91f81b4cddff6d3888f31284774
sushantyerawar1 <84599862+sushantyerawar1@users.noreply.github.com>
2022-10-27 15:46:49 +0530

    Connected GithubTable with backend

--- app/src/components/GithubTable.js
+++ app/src/components/GithubTable.js
@@ -14,6 +14,7 @@ const useStyles = makeStyles({
 
 export const GithubTable = ({ githubUser }) => {
     const classes = useStyles();
+
     return (
         <div className="openlake" style={{ display: "flex", justifyContent: "center", alignItems: "center", marginTop: "8px", paddingLeft: "100%", paddingRight: "100%" }}>
             <div>
@@ -28,14 +29,17 @@ export const GithubTable = ({ githubUser }) => {
                                 <TableCell >Last_updated</TableCell>
                             </TableRow>
                         </TableHead>
-                        {/* <TableBody>
-                {githubUser.map(glUser => (
-                    <TableRow key={glUser.id}>
-                        <TableCell>{glUser.Contributor}</TableCell>
-                        <TableCell>{glUser.Contriution}</TableCell>
-                    </TableRow>
-                ))}
-            </TableBody> */}
+                        <TableBody>
+                            {githubUser.map(glUser => (
+                                <TableRow key={glUser.id}>
+                                    <TableCell>{glUser.username}</TableCell>
+                                    <TableCell>{glUser.contributions}</TableCell>
+                                    <TableCell>{glUser.repositories}</TableCell>
+                                    <TableCell>{glUser.stars}</TableCell>
+                                    <TableCell>{glUser.last_updated}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
                     </Table>
                 </TableContainer>
             </div>
@@ -44,46 +48,3 @@ export const GithubTable = ({ githubUser }) => {
 
 }
 
-const MyResponsiveLine = ({ url }) => {
-
-    const [ratingUpdates, setRatingUpdates] = useState([]);
-
-    useEffect(() => {
-        fetch(url)
-            .then(res => res.json())
-            .then(res => {
-                const updates = res["rating_updates"].map(entry => ({ x: entry.rating, y: entry.timestamp }));
-                console.log(updates);
-                setRatingUpdates([
-                    {
-                        "id": `data_${url}`,
-                        "color": "hsl(28, 70%, 50%)",
-                        "data": updates
-                    }
-                ])
-            })
-    }, [url])
-
-    return (
-        <ResponsiveLine
-            data={ratingUpdates}
-            xScale={{ type: 'point' }}
-            yScale={{ type: 'linear', min: 'auto', max: 'auto', stacked: true, reverse: false }}
-            axisTop={null}
-            axisRight={null}
-            axisBottom={null}
-            axisLeft={null}
-            enableGridX={false}
-            enableGridY={false}
-            enablePoints={false}
-            pointSize={10}
-            colors={{ scheme: 'category10' }}
-            pointColor={{ theme: 'background' }}
-            pointBorderWidth={2}
-            pointBorderColor={{ from: 'serieColor' }}
-            // pointLabelYOffset={-12}
-            useMesh={true}
-            legends={[]}
-        />
-    )
-}
