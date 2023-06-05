25eba2137f43cfe1268a16f916d78208a4da9abc
Aditya062003 <adityavinay@iitbhilai.ac.in>
2023-06-05 13:38:29 +0530

    Added some requirements

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn 'leaderboard.wsgi'
--- api/leaderboard/settings.py
+++ api/leaderboard/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = (
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
