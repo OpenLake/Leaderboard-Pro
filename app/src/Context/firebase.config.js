11be8be998dbabd917a7c6e8c7aac784b399eb43
sumagnadas <sumagnadas@gmail.com>
2025-07-03 13:06:54 +0530

    Fix VITE_* security issue being shown in Vercel about those variables being exposed publicly to the browser.

--- app/src/Context/firebase.config.js
+++ app/src/Context/firebase.config.js
@@ -1,19 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// import "dotenv/config";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
+  apiKey: API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
+  projectId: PROJECT_ID,
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
   measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
